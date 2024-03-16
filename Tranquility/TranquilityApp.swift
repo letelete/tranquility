21e3031f7c7b6ffbe6760d6dd9961c6f673a86ba
letelete <brunokawka@gmail.com>
2024-03-16 11:39:40 +0100

    Initial Commit

---
+++ Tranquility/TranquilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranquilityApp.swift
+//  Tranquility
+//
+//  Created by Bruno Kawka on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TranquilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
